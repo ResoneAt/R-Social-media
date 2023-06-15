44bd6ba940b307706c4338dc5b39aa14a0875124
Ramin <eslamiramin85@gmail.com>
2023-06-15 13:20:56 +0330

    add MessageModel & NotificationsModel & MovieModel and modified models

--- posts/models.py
+++ posts/models.py
@@ -21,6 +21,14 @@ class ImagePostModel(models.Model):
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='image')
     alt = models.CharField(max_length=73)
 
+    def __str__(self):
+        return self.alt
+
+
+class MoviePostModel(models.Model):
+    movie = models.FileField(upload_to='posts_movie')
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+
 
 class ReportPostModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='reporter')
@@ -28,15 +36,26 @@ class ReportPostModel(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.post} - {self.body[:20]}'
+
 
 class LikeModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liker')
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='liked_post')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.post}-{self.user.username}'
+
 
 class CommentModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_author')
+    body = models.TextField()
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comment')
     created_at = models.DateTimeField(auto_now_add=True)
-    replay_to = models.ForeignKey('self', on_delete=models.PROTECT)
+    replay_to = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    is_replay = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.author.username} - {self.body[:20]} - {self.post}'
