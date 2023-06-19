439c703bc50edcd8c5ac5023932553d51a9523f4
Ramin <eslamiramin85@gmail.com>
2023-06-20 02:26:55 +0330

    Adding names of the views I guess I should write for posts in my project

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class CreatePostView(View):
+    ...
+
+
+class EditPostView(View):
+    ...
+
+
+class DeletePostView(View):
+    ...
+
+
+class DetailPostView(View):
+    ...
+
+
+class LikePostView(View):
+    ...
+
+
+class DislikePostView(View):
+    ...
+
+
+class SentCommentView(View):
+    ...
+
+
+class EditCommentView(View):
+    ...
+
+
+class DeleteCommentView(View):
+    ...
+
+
+class ReportPostView(View):
+    ...
+
+
+class SavePostToWishList(View):
+    ...
+
