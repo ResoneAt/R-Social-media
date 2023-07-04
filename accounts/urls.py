71bad281bd339667f73720b1d6b4de62dea7fb1f
Ramin <eslamiramin85@gmail.com>
2023-07-05 01:49:59 +0330

    add google authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from .views import SignupView, LoginView, LogoutView,\
 
 app_name = 'accounts'
 urlpatterns = [
-    path('accounts/signup/', SignupView.as_view(), name='signup'),
+    path('accounts/signup-user/', SignupView.as_view(), name='signup'),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('accounts/profile/<int:user_id>/', ProfileView.as_view(), name='user_profile'),
