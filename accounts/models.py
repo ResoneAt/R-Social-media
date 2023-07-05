53fe2c7a099982e39b327bd7ce98867d5334bc66
Ramin <eslamiramin85@gmail.com>
2023-07-05 14:12:33 +0330

    style

--- accounts/models.py
+++ accounts/models.py
@@ -86,53 +86,49 @@ class User(AbstractBaseUser):
         return self.request_receive.count()
 
     def is_following(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         return self.follower.filter(to_user=user).exists()
 
     def is_follow_requesting(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         return self.request_sent.filter(to_user=user).exists()
 
     def is_followed_by(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         return self.following.filter(from_user=user).exists()
 
     def follow(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         relation = RelationModel(from_user=self, to_user=user)
         relation.save()
 
     def unfollow(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         relation = RelationModel.objects.get(from_user=self, to_user=user)
         relation.delete()
 
     def follow_request(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         request = FollowRequestModel(from_user=self, to_user=user)
         request.save()
 
     def accept_follow_request(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         request = FollowRequestModel.objects.get(from_user=user, to_user=self)
         request.delete()
         User.follow(user, self.pk)
 
     def reject_follow_request(self, user_id):
-        user = User.objects.get(pk=user_id)
+        user = self.user_instance(pk=user_id)
         request = FollowRequestModel.objects.get(from_user=user, to_user=self)
         request.delete()
 
-
-    @staticmethod
-    def is_privet(user_id):
-        user = User.objects.get(pk=user_id)
+    def is_privet(self,user_id):
+        user = self.user_instance(pk=user_id)
         return user.account_type == 'privet'
 
-
-    @staticmethod
-    def get_follower_list(user_id):
-        user = User.objects.get(pk=user_id)
+    def get_follower_list(self, user_id):
+        user = self.user_instance(pk=user_id)
         return User.objects.filter(following__to_user=user)
 
     @staticmethod
